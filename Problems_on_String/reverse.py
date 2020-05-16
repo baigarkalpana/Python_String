1575eecc31d4f1b8f4108a17788973b2818590f2
baigarkalpana <baigarkalpana96@gmail.com>
2020-05-17 00:18:52 +0530

    Add files via upload

---
+++ Problems_on_String/reverse.py
@@ -0,0 +1,22 @@
+'''
+Author:Kalpana Baigar
+Program which contains one funtion that accept string from user and return return reverse of
+that string
+
+'''
+
+str=input("enter string: ")
+
+print("your entered string is:"+str)
+
+no=len(str)
+def reverse(arr):
+
+    for i in range()
+
+
+
+
+
+
+reverse(str)
